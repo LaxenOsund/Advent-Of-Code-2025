464a1f3a6a300b9a438d3d246be9ad1981b38392
Felix Ösund <felos046@student.liu.se>
2025-12-12 13:32:37 +0100

    day3.5 completed

--- Day3/day3_5.py
+++ Day3/day3_5.py
@@ -5,48 +5,49 @@ test_data = [
 [234234234234278],
 [818181911112111]
 ]
+
 tot_joltage = 0
 #Skapar lista med listor
-with open("Day3\Day3input.txt") as f:
+with open("Day3/Day3input.txt") as f:
   for line in f:
     input_data.append(line.strip().split(","))
 #print(input_data)
 
 for lists in input_data:
-  
-  list_string = str(lists)[2:102]
-  print(list_string)
-  print(str(lists))
+  offset = -11
+  offset2 = 0
+  string_joltage = ""
+  list_string = str(lists[0])
   #Finds Ten digit
-  for n1 in range(9,0,-1):
-
-    try:
-      res = list_string.index(str(n1))
-      print(f"{res} == {len(list_string)}" )
-      if res == len(list_string)-1:
-        continue
-      b1 = list_string[res]
-      print(f"Tiotal är: {list_string[res]}")
-      lists = list_string[res+1:]
-      #print(f"Index for biggest number: {res}. List now looks like \n {lists}")
-      break
-    except ValueError:
-      print(f"Number {n1} is not in current list")
+  for i in range(0,12):
     
-  for n2 in range(9,0,-1):
-    try:
-      res2 = lists.index(str(n2))
+    for n in range(9,0,-1):
+      number = ""
+      try:
 
-      b2 = lists[res2]
-      print(f"Ental är: {b2}")
-      break
-    except ValueError:
-      print(f"Number {n2} is not in current list")
+        #Ensuring slicing works as intended since slicing with 0 just gives a empty list 
+        if offset == 0:
+          active_list = list_string[offset2:]
+        else:
+          active_list = list_string[offset2:offset]
+
+        res = active_list.index(str(n))
+        number = active_list[res]
+        print(number)
+        #print(f"Ental är: {b2}")
+        break
+      except ValueError:
+        #print(f"Number {n2} is not in current list")
+        continue
+    offset += 1
+    offset2 += res +1
+    
  
-  string_joltage = b1 + b2
-  print(f"joltage for this battery is {int(string_joltage)}")
-  print(f"total joltage is now {int(string_joltage)} + {int(tot_joltage)} = {int(string_joltage) + int(tot_joltage)}")
+    string_joltage += number 
+ # print(f"joltage for this battery is {int(string_joltage)}")
+  #print(f"total joltage is now {int(string_joltage)} + {int(tot_joltage)} = {int(string_joltage) + int(tot_joltage)}")
+  print(string_joltage)
   tot_joltage += int(string_joltage)
-  
-  print 
 print(tot_joltage)
+  
+#print(tot_joltage)
